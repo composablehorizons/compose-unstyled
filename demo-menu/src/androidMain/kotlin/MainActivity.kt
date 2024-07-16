f880025111362960670e4df40ae0f57b0acd361e
alexstyl <1665273+alexstyl@users.noreply.github.com>
2024-07-16 13:20:40 +0300

    Remove systemPadding from demo

--- demo-menu/src/androidMain/kotlin/MainActivity.kt
+++ demo-menu/src/androidMain/kotlin/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.composables.core.demo
 
-import MenuDemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +13,7 @@ class MainActivity : ComponentActivity() {
         actionBar?.hide()
 
         setContent {
-            Box(Modifier.systemBarsPadding()) {
-                MenuDemo()
-            }
+            MenuDemo()
         }
     }
 }
