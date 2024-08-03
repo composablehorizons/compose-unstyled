fe30470c567763179e159740f622adc21fa52632
alexstyl <1665273+alexstyl@users.noreply.github.com>
2024-08-03 09:13:08 +0300

    Enable rememberSavable within Modals

--- core/src/androidMain/kotlin/Modal.android.kt
+++ core/src/androidMain/kotlin/Modal.android.kt
@@ -5,22 +5,35 @@ import androidx.activity.ComponentDialog
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.rememberCompositionContext
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import androidx.lifecycle.setViewTreeLifecycleOwner
+import androidx.lifecycle.setViewTreeViewModelStoreOwner
+import androidx.savedstate.findViewTreeSavedStateRegistryOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+import java.util.UUID
 
 @Composable
 internal actual fun Modal(
     protectNavBars: Boolean,
     content: @Composable () -> Unit
 ) {
+    val parentView = LocalView.current
     val context = LocalContext.current
     val composition = rememberCompositionContext()
-    DisposableEffect(Unit) {
+    val id = rememberSaveable { UUID.randomUUID() }
+
+    DisposableEffect(parentView) {
         val contentView = ComposeView(context).apply {
+            setTag(androidx.compose.ui.R.id.compose_view_saveable_id_tag, "modal_$id")
             setParentCompositionContext(composition)
             setContent(content)
         }
@@ -36,6 +49,10 @@ internal actual fun Modal(
             window.setWindowAnimations(-1)
 
             setContentView(contentView)
+
+            contentView.setViewTreeLifecycleOwner(parentView.findViewTreeLifecycleOwner())
+            contentView.setViewTreeViewModelStoreOwner(parentView.findViewTreeViewModelStoreOwner())
+            contentView.setViewTreeSavedStateRegistryOwner(parentView.findViewTreeSavedStateRegistryOwner())
 
             setCancelable(false)
             setCanceledOnTouchOutside(false)
