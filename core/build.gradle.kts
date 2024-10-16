bdd3bcaf6e2b17f55457fbe0e89da61d047cad7e
alexstyl <1665273+alexstyl@users.noreply.github.com>
2024-10-17 01:50:47 +0700

    Prepare 1.16.1

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 val publishGroupId = "com.composables"
 val publishArtifactId = "core"
-val publishVersion = "1.17.0"
+val publishVersion = "1.16.1"
 val githubUrl = "github.com/composablehorizons/composables-core"
 
 java {
