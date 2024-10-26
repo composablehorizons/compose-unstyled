689fefb0b582c2ab10db98a6a3d68f0347cba553
alexstyl <1665273+alexstyl@users.noreply.github.com>
2024-10-26 17:19:54 +0700

    Remove unused dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -70,7 +70,6 @@ kotlin {
             }
         }
         androidMain.dependencies {
-            implementation("androidx.activity:activity:1.9.0")
             implementation("androidx.activity:activity-compose:1.9.0")
         }
         jvmMain.dependencies {
