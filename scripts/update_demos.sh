8fb2bb4487c7dca6cc68c9c1df8eb8bab2288433
alexstyl <1665273+alexstyl@users.noreply.github.com>
2025-04-19 17:15:07 +0300

    Remove hardcoded demo names from update_demos.sh

--- scripts/update_demos.sh
+++ scripts/update_demos.sh
@@ -1,21 +1,32 @@
 # Script that builds the JS version of the demo and moves it to the docs resources
 
-project_root=$(pwd)
-demo_names=("dialog" "icon" "menu" "modalsheet" "sheet" "separators" "scrollarea")
-
-# Create directories for each demo
-for demo in "${demo_names[@]}"; do
-    demo_destination="${project_root}/docs/${demo}"
-    mkdir -p "$demo_destination-demo"
-    rm -rf "$demo_destination-demo"/*
-done
-
 ## Build the project
 ./gradlew jsBrowserDevelopmentExecutableDistribution
 
+project_root=$(pwd)
+
+# Get all directories starting with demo- and exclude demo-ios
+all_demo_dirs=($(find . -maxdepth 1 -type d -name 'demo-*' ! -name 'demo-ios'))
+
+demo_names=()
+for dir in "${all_demo_dirs[@]}"; do
+    demo_name=$(basename "$dir")
+    demo_names+=("${demo_name#demo-}")
+    # Remove the 'demo-' prefix
+    demo_names+=("${demo_name#demo-}")
+done
+
+# Create directories for each demo
+for demo in "${demo_names[@]}"; do
+    demo_destination="${project_root}/docs/demo-${demo}"
+    echo $demo_destination
+    mkdir -p "$demo_destination"
+    rm -rf "$demo_destination"/*
+done
+
 ## Move built files to respective directories
 for demo in "${demo_names[@]}"; do
-    demo_source="${project_root}/demo-${demo%-demo}/build/dist/js/developmentExecutable/*"
-    demo_destination="${project_root}/docs/${demo}-demo"
+    demo_source="${project_root}/demo-${demo}/build/dist/js/developmentExecutable/*"
+    demo_destination="${project_root}/docs/demo-${demo}"
     mv $demo_source "$demo_destination"
 done
