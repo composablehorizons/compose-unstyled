2a73a5890456e683f90f36e6ac7abd09b9dcfb9a
alexstyl <1665273+alexstyl@users.noreply.github.com>
2025-05-13 16:48:23 +0300

    Use LightIndication as indication on iOS demo

--- demo/src/iosMain/kotlin/iOSMain.kt
+++ demo/src/iosMain/kotlin/iOSMain.kt
@@ -1,7 +1,11 @@
 package com.composeunstyled.demo
 
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.window.ComposeUIViewController
 
 fun MainViewController() = ComposeUIViewController {
-    Demo()
+    CompositionLocalProvider(LocalIndication provides LightIndication) {
+        Demo()
+    }
 }
