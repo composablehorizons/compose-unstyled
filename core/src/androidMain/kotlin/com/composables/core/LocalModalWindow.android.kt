9da1317266b40e66808327fe67bbc6ec5d35d03f
alexstyl <1665273+alexstyl@users.noreply.github.com>
2025-06-22 22:52:01 +0300

    Remove deprecated LocalModalWindow from com.composables.core package

--- core/src/androidMain/kotlin/com/composables/core/LocalModalWindow.android.kt
+++
@@ -1,9 +0,0 @@
-package com.composables.core
-
-import com.composeunstyled.LocalModalWindow as UnstyledModalWindow
-
-@Deprecated(
-    "LocalModalWindow was moved to the com.composeunstyled package",
-    ReplaceWith("com.composeunstyled.LocalModalWindow")
-)
-val LocalModalWindow = UnstyledModalWindow
