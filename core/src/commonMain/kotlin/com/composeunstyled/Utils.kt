402ffa2aa532a194777c55b060f41b1f31a62142
alexstyl <1665273+alexstyl@users.noreply.github.com>
2025-07-24 11:13:00 +0700

    Introduce ProvideContentColor and ProvideTextStyle

--- core/src/commonMain/kotlin/com/composeunstyled/Utils.kt
+++ core/src/commonMain/kotlin/com/composeunstyled/Utils.kt
@@ -6,6 +6,8 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,6 +23,16 @@ internal val NoPadding = PaddingValues(0.dp)
 
 val LocalContentColor = compositionLocalOf { Color.Unspecified }
 val LocalTextStyle = compositionLocalOf { TextStyle.Default }
+
+@Composable
+fun ProvideContentColor(color: Color, content: @Composable () -> Unit) {
+    CompositionLocalProvider(LocalContentColor provides color, content = content)
+}
+
+@Composable
+fun ProvideTextStyle(textStyle: TextStyle, content: @Composable () -> Unit) {
+    CompositionLocalProvider(LocalTextStyle provides textStyle, content = content)
+}
 
 internal val KeyEvent.isKeyDown: Boolean
     get() = type == KeyEventType.KeyDown
