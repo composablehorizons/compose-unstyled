f4790e63536678bf91c99d4ad6a0fab316b5436f
alexstyl <1665273+alexstyl@users.noreply.github.com>
2025-10-19 14:16:28 +0700

    Test Modal component

--- core/src/jvmTest/kotlin/Modal.test.kt
+++ core/src/jvmTest/kotlin/Modal.test.kt
@@ -8,48 +8,28 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.isDialog
 import androidx.compose.ui.test.runComposeUiTest
 import androidx.compose.ui.unit.dp
-import com.composeunstyled.Button
 import com.composeunstyled.Modal
 import kotlin.test.Test
-import org.assertj.core.api.Assertions.assertThat
 
 @OptIn(ExperimentalTestApi::class)
 class ModalTest {
 
     @Test
-    fun modalInComposition_blocksInteractionsWithBelowLayer() = runComposeUiTest {
+    fun isModal() = runComposeUiTest {
         var showModal by mutableStateOf(false)
-        var clickCount by mutableStateOf(0)
 
         setContent {
-            Button(
-                onClick = { clickCount++ },
-                modifier = Modifier
-                    .testTag("button")
-                    .size(40.dp)
-            ) {
-
-            }
-
             if (showModal) {
                 Modal(onKeyEvent = { false }) {
                     Box(Modifier.testTag("modal_contents").size(40.dp))
                 }
             }
         }
-
-        onNodeWithTag("button").performClick()
-
-        assertThat(clickCount).isEqualTo(1)
-
+        onNode(isDialog()).assertDoesNotExist()
         showModal = true
-
-        onNodeWithTag("button").performClick()
-
-        assertThat(clickCount).isEqualTo(1)
+        onNode(isDialog()).assertExists()
     }
 }
