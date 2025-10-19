e27b8b1bcaf34f3be1f0196e71e9d23cfa433fc1
alexstyl <1665273+alexstyl@users.noreply.github.com>
2025-10-19 12:50:37 +0700

    Test Dialog

--- core/src/jvmTest/kotlin/Dialog.test.kt
+++ core/src/jvmTest/kotlin/Dialog.test.kt
@@ -2,20 +2,24 @@ package com.composeunstyled
 
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.assertIsFocused
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.test.*
+import androidx.compose.ui.unit.dp
 import com.composables.core.Dialog
 import com.composables.core.DialogPanel
+import com.composables.core.DialogState
 import com.composables.core.rememberDialogState
 import kotlin.test.Test
+import org.assertj.core.api.Assertions.assertThat
 
+@OptIn(ExperimentalTestApi::class)
 class DialogTest {
 
-    @OptIn(ExperimentalTestApi::class)
     @Test
     fun focusesContentWhenDisplayed() = runComposeUiTest {
         setContent {
@@ -27,5 +31,40 @@ class DialogTest {
         }
 
         onNodeWithTag("dialog_content").assertIsFocused()
+    }
+
+    @Test
+    fun dialogIsModal() = runComposeUiTest {
+        var clickCount by mutableStateOf(0)
+        lateinit var dialogState: DialogState
+
+        setContent {
+            dialogState = rememberDialogState(initiallyVisible = false)
+
+            Button(
+                onClick = { clickCount++ },
+                modifier = Modifier
+                    .testTag("button")
+                    .size(40.dp)
+            ) {
+
+            }
+
+            Dialog(dialogState) {
+                DialogPanel {
+                    Box(Modifier.testTag("dialog_content").size(40.dp))
+                }
+            }
+        }
+
+        onNodeWithTag("button").performClick()
+
+        assertThat(clickCount).isEqualTo(1)
+
+        dialogState.visible = true
+
+        onNodeWithTag("button").performClick()
+
+        assertThat(clickCount).isEqualTo(1)
     }
 }
