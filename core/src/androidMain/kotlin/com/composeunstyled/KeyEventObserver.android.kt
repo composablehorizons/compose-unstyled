b849509e6de50df1de1380830712a5317b23ec33
alexstyl <1665273+alexstyl@users.noreply.github.com>
2025-10-27 11:42:28 +0700

    Allow modals to be dismissed by Backpress prior to Android 28 alexstyl 35 minutes ago

--- core/src/androidMain/kotlin/com/composeunstyled/KeyEventObserver.android.kt
+++ core/src/androidMain/kotlin/com/composeunstyled/KeyEventObserver.android.kt
@@ -2,9 +2,11 @@ package com.composeunstyled
 
 import android.os.Build
 import android.view.View
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.input.key.NativeKeyEvent
 import androidx.compose.ui.platform.LocalView
 
 @Composable
@@ -19,6 +21,11 @@ internal actual fun KeyEventObserver(onEvent: (KeyEvent) -> Unit) {
             }
             view.addOnUnhandledKeyEventListener(listener)
             onDispose { view.removeOnUnhandledKeyEventListener(listener) }
+        }
+    } else {
+        BackHandler {
+            onEvent(KeyEvent(NativeKeyEvent(NativeKeyEvent.ACTION_DOWN, NativeKeyEvent.KEYCODE_BACK)))
+            onEvent(KeyEvent(NativeKeyEvent(NativeKeyEvent.ACTION_UP, NativeKeyEvent.KEYCODE_BACK)))
         }
     }
 }
