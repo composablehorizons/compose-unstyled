1049078180490d0e5ab5df8747d1dda978766533
alexstyl <1665273+alexstyl@users.noreply.github.com>
2025-10-29 19:15:26 +0700

    Remove log

--- core/src/commonMain/kotlin/BottomSheet.kt
+++ core/src/commonMain/kotlin/BottomSheet.kt
@@ -504,7 +504,6 @@ fun BottomSheet(
                         .sheetOffset(state = state, imeAware = imeAware)
                 )
                 if (scope.enabled && state.detents.size > 1) {
-                    println("SHEET GOT enabled = ${scope.enabled}")
                     add(
                         Modifier
                             .unstyledAnchoredDraggable(
