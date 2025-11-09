15dee8473ddde0ac6f8649b06c35f06f4dc0d82b
alexstyl <1665273+alexstyl@users.noreply.github.com>
2025-11-09 14:36:09 +0700

    Configure CI for multi-module setup

--- composeunstyled-primitives/build.gradle.kts
+++ composeunstyled-primitives/build.gradle.kts
@@ -1,6 +1,7 @@
 @file:Suppress("UnstableApiUsage")
 @file:OptIn(ExperimentalKotlinGradlePluginApi::class)
 
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
@@ -9,6 +10,7 @@ plugins {
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
+    alias(libs.plugins.detekt)
 }
 
 java {
@@ -111,5 +113,31 @@ android {
     compileSdk = libs.versions.android.compileSDK.get().toInt()
     defaultConfig {
         minSdk = libs.versions.android.minSDK.get().toInt()
+    }
+}
+
+val detektSourceDirs = listOf(
+    "src/commonMain/kotlin",
+    "src/cmpMain/kotlin",
+    "src/androidMain/kotlin",
+    "src/jvmMain/kotlin",
+    "src/iosMain/kotlin",
+    "src/webMain/kotlin"
+).map(::file).filter(File::exists)
+
+detekt {
+    buildUponDefaultConfig = true
+    config.setFrom(files(rootProject.file("detekt.yml")))
+    parallel = true
+    source.setFrom(detektSourceDirs)
+}
+
+tasks.withType<Detekt>().configureEach {
+    jvmTarget = JvmTarget.JVM_17.target
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+        txt.required.set(true)
+        sarif.required.set(false)
     }
 }
