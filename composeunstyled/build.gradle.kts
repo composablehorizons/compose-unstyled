348c76e53101749f2ded743d13e4a114260e306d
alexstyl <1665273+alexstyl@users.noreply.github.com>
2025-12-15 14:37:01 +0200

    Update support email

--- composeunstyled/build.gradle.kts
+++ composeunstyled/build.gradle.kts
@@ -99,7 +99,7 @@ mavenPublishing {
             developer {
                 id.set("composablehorizons")
                 name.set("Composable Horizons")
-                email.set("alex@composablesui.com")
+                email.set("support@composables.com")
             }
         }
 
