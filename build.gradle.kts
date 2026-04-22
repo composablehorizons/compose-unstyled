9e4969c7d4ca8b0a7f5d7b19cae0ed27277fd2e2
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-04-22 17:40:53 +0700

    Add Spotless + ktlint setup and editorconfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,38 @@ plugins {
     alias(libs.plugins.compose.hotreload) apply false
     alias(libs.plugins.maven.publish)
     alias(libs.plugins.detekt).apply(false)
+    alias(libs.plugins.spotless)
+}
+
+subprojects {
+    apply(plugin = rootProject.libs.plugins.spotless.get().pluginId)
+
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target(
+                fileTree(project.projectDir) {
+                    include("src/**/*.kt")
+                    exclude("src/**/kotlin/androidx/**/*.kt")
+                }
+            )
+            ktlint().editorConfigOverride(
+                mapOf(
+                    "indent_size" to 2,
+                    "continuation_indent_size" to 2,
+                    "ktlint_standard_filename" to "disabled",
+                )
+            )
+            licenseHeaderFile(rootProject.file("$rootDir/spotless/copyright.kt"))
+        }
+        format("kts") {
+            target("**/*.kts")
+            targetExclude("**/build/**/*.kts")
+            licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
+        }
+        format("xml") {
+            target("**/*.xml")
+            targetExclude("**/build/**/*.xml")
+            licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
+        }
+    }
 }
