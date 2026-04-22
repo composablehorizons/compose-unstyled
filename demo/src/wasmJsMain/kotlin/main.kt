b132b2386d1f103284fcb26833596bc268f92fca
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-04-22 15:07:22 +0700

    Clean up wasm demo main

--- demo/src/wasmJsMain/kotlin/main.kt
+++ demo/src/wasmJsMain/kotlin/main.kt
@@ -1,12 +1,9 @@
 @file:OptIn(ExperimentalWasmJsInterop::class)
 
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import com.composeunstyled.demo.Demo
 
-@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    println("Start main")
     ComposeViewport {
         Demo()
     }
