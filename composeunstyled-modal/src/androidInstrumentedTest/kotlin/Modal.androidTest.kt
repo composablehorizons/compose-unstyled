2fa018f33c560565b856b12c3c74d330b0ec4ca9
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-05-02 08:17:26 +0700

    Fix Spotless formatting in modal Android test

--- composeunstyled-modal/src/androidInstrumentedTest/kotlin/Modal.androidTest.kt
+++ composeunstyled-modal/src/androidInstrumentedTest/kotlin/Modal.androidTest.kt
@@ -24,7 +24,6 @@ package com.composeunstyled
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
