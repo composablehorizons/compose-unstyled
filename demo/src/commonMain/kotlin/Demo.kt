27b3a693e623afbffd97d9ea6b5fd33f7c01b4e9
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-05-09 20:37:14 +0700

    Remove Haze demos

--- demo/src/commonMain/kotlin/Demo.kt
+++ demo/src/commonMain/kotlin/Demo.kt
@@ -138,8 +138,6 @@ private val availableModifiers = listOf(
   DemoItem("Outline Shape", "outline-shape", { OutlineShapeDemo() }),
   DemoItem("Outline Offset", "outline-offset", { OutlineOffsetDemo() }),
   DemoItem("Outline Color", "outline-color", { OutlineColorDemo() }),
-  DemoItem("Haze", "haze", { HazeDemo() }),
-  DemoItem("Haze Scrim", "haze-scrim", { HazeScrimDemo() }),
   DemoItem("Focus Ring Basic", "focus-ring-basic", { FocusRingBasicDemo() }),
   DemoItem("Focus Ring Width", "focus-ring-width", { FocusRingWidthDemo() }),
   DemoItem("Focus Ring Shape", "focus-ring-shape", { FocusRingShapeDemo() }),
