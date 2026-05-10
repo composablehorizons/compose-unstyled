67b347bff631444427efbc0dbfd8b3aa1f26990a
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-05-10 10:35:22 +0700

    Format bottom sheet demo imports

--- demo/src/commonMain/kotlin/BottomSheetDemo.kt
+++ demo/src/commonMain/kotlin/BottomSheetDemo.kt
@@ -35,8 +35,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
