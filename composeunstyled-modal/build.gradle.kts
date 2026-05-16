926b0f38899816e0ef398fe66ec6dd838cb884a8
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-05-16 15:53:52 +0700

    Add modal recomposition performance test

--- composeunstyled-modal/build.gradle.kts
+++ composeunstyled-modal/build.gradle.kts
@@ -95,6 +95,7 @@ kotlin {
 
     commonTest.dependencies {
       implementation(kotlin("test"))
+      implementation(projects.composeunstyledTest)
 
       @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
       implementation(libs.compose.ui.test)
