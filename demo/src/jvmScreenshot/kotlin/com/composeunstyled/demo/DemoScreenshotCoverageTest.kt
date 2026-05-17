0e45f01afbfead1cd106e2cf3c9d67599cde6120
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-05-17 17:27:18 +0700

    Add screenshot coverage for remaining demos

---
+++ demo/src/jvmScreenshot/kotlin/com/composeunstyled/demo/DemoScreenshotCoverageTest.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2026 Composable Horizons
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.composeunstyled.demo
+
+import kotlin.test.Test
+
+class DemoScreenshotCoverageTest {
+
+  @Test
+  fun buttonDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(ButtonDemoScreenshot)
+  }
+
+  @Test
+  fun checkboxDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(CheckboxDemoScreenshot)
+  }
+
+  @Test
+  fun triStateCheckboxDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(TriStateCheckboxDemoScreenshot)
+  }
+
+  @Test
+  fun dialogDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(DialogDemoScreenshot)
+  }
+
+  @Test
+  fun disclosureDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(DisclosureDemoScreenshot)
+  }
+
+  @Test
+  fun dropdownMenuDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(DropdownMenuDemoScreenshot)
+  }
+
+  @Test
+  fun progressIndicatorDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(ProgressIndicatorDemoScreenshot)
+  }
+
+  @Test
+  fun radioGroupDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(RadioGroupDemoScreenshot)
+  }
+
+  @Test
+  fun scrollbarsDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(ScrollbarsDemoScreenshot)
+  }
+
+  @Test
+  fun separatorsDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(SeparatorsDemoScreenshot)
+  }
+
+  @Test
+  fun sliderDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(SliderDemoScreenshot)
+  }
+
+  @Test
+  fun tabGroupDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(TabGroupDemoScreenshot)
+  }
+
+  @Test
+  fun textFieldDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(TextFieldDemoScreenshot)
+  }
+
+  @Test
+  fun toggleSwitchDemoMatchesScreenshot() {
+    assertDemoScreenshotMatches(ToggleSwitchDemoScreenshot)
+  }
+}
