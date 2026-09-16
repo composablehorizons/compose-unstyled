b34f2916d7d33b5511d38e8f1df12b963acac656
alexstyl <1665273+alexstyl@users.noreply.github.com>
2026-09-16 14:23:16 +0700

    Reduce development demo build output

--- website/scripts/dev.js
+++ website/scripts/dev.js
@@ -59,7 +59,7 @@ const buildDemo = () => {
   console.log('Building development demo…');
   demoBuild = spawn(
     './gradlew',
-    [':demo:wasmJsBrowserDevelopmentExecutableDistribution', '--console=plain', '--configuration-cache'],
+    [':demo:wasmJsBrowserDevelopmentExecutableDistribution', '--console=plain', '--warn', '--configuration-cache'],
     { cwd: root, stdio: 'inherit' },
   );
   demoBuild.on('close', (code) => {
